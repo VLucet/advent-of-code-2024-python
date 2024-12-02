2c4c72e01bbbf07ecea17d896244caef19542353
vlucet <valentin.lucet@gmail.com>
2024-12-02 18:00:19 -0500

    day 1 done

---
+++ code/day1.py
@@ -0,0 +1,36 @@
+# Libraries
+import pandas as pd
+
+# Importing data
+ex1 = pd.read_csv('data/day1-ex1.txt', sep='   ', engine='python', header=None, 
+                  names=["a", "b"])
+print(ex1.head())
+
+dat = pd.read_csv('data/day1-dat.txt', sep='   ', engine='python', header=None, 
+                  names=["a", "b"])
+print(dat.head())
+
+# Part 1
+# ------
+
+def sort_distance_sum (df):
+    df_sorted = df.apply(lambda col: col.sort_values().values, axis=0)
+    df_distanced = df_sorted.apply(lambda vals: abs(vals.iloc[0] - vals.iloc[1]), 
+                                   axis=1)
+    return sum(df_distanced)
+
+sort_distance_sum(ex1) == 11
+sort_distance_sum(dat)
+
+# Part 2
+# ------
+
+from collections import Counter
+
+def similarity_score (df):
+    counts = Counter(df.b)
+    score = sum(df.a.apply(lambda x: counts[x] * x))
+    return score
+
+similarity_score(ex1) == 31
+similarity_score(dat)
